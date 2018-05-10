f1f43204f10517011aa3b840310ed9392a2d45c1
Ryan Cooper <rcooper@edwardsaquifer.org>
2018-05-10 14:11:12 -0500

    Initial commit

---
+++ Util.cs
@@ -0,0 +1,22 @@
+﻿using System.IO;
+
+namespace HoboLib
+{
+    public static class Util
+    {
+        /// <summary>
+        /// Generates a stream containing string data
+        /// </summary>
+        /// <param name="val">The text to put in the stream</param>
+        /// <returns>A Stream object</returns>
+        public static Stream GetStreamFromString(string val)
+        {
+            var stream = new MemoryStream();
+            var writer = new StreamWriter(stream);
+            writer.Write(val);
+            writer.Flush();
+            stream.Position = 0;
+            return stream;
+        }
+    }
+}
